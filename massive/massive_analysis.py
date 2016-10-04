402b01c4a194ee76df7157717b0c752ada2874eb
sgongar <s.gongoragarcia@gmail.com>
2016-10-04 13:34:18 +0200

    Generic massive analisys script improved

--- massive/massive_analysis.py
+++ massive/massive_analysis.py
@@ -1,35 +1,48 @@
+from os import getenv, path, mkdir
+from time import time
+import datetime
+from csv import reader
+import string
+import shutil
+
 #  coding = utf-8
-# 
+#
 #  This file is part of Herschel Common Science System (HCSS).
 #  Copyright 2001-2013 Herschel Science Ground Segment Consortium
-# 
+#
 #  HCSS is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as
 #  published by the Free Software Foundation, either version 3 of
 #  the License, or (at your option) any later version.
-# 
+#
 #  HCSS is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #  GNU Lesser General Public License for more details.
-# 
+#
 #  You should have received a copy of the GNU Lesser General
 #  Public License along with HCSS.
 #  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 
-import os
-import time
-import datetime
-import csv
-import string
-import shutil
 
-home_dir = os.getenv("HOME")
+def get_formatted_time():
+    """ Return formatted time function
+
+    """
+    time_hr = datetime.datetime.fromtimestamp(time())
+    time_hr = str(time_hr)
+
+    return time_hr
+
+
+# Load directories
+# In our case we assumed that ...
+
+home_dir = getenv("HOME")
 working_dir = str(home_dir) + '/hcss/workspace/Red-leak/'
 pool_dir = str(working_dir) + 'pools/'
 zips_dir = str(pool_dir) + 'zips/'
-plot_dir = str(working_dir) + 'plots/'
 csv_obs = str(working_dir) + 'obs_ids.csv'
 
 save_obs = False
@@ -43,20 +56,18 @@ start_time = time.time()
 start_time_hr = datetime.datetime.fromtimestamp(start_time)
 start_time_hr = str(start_time_hr)
 
-if (not os.path.exists(working_dir)):
-    os.mkdir(working_dir)
-if (not os.path.exists(pool_dir)):
-    os.mkdir(pool_dir)
-if (not os.path.exists(zips_dir)):
-    os.mkdir(zips_dir)
-if (not os.path.exists(plot_dir)):
-    os.mkdir(plot_dir)
+if (not path.exists(working_dir)):
+    mkdir(working_dir)
+if (not path.exists(pool_dir)):
+    mkdir(pool_dir)
+if (not path.exists(zips_dir)):
+    mkdir(zips_dir)
 
 # Populate list from csv file
 obs_list = []
 with open(str(csv_obs), 'rb') as f:
-    reader = csv.reader(f, delimiter=',')
-    for row in reader:
+    row_reader = reader(f, delimiter=',')
+    for row in row_reader:
         obs_list.append(row[1])
 
 # Create dictionary from list
@@ -83,7 +94,7 @@ for i in range(len(obs_list)):
 # Create file for tracking the progress
 trackfilename = working_dir + "RedLeakMultiObs.txt"
 trackfile = open(trackfilename, 'w')
-trackfile.write("Starting process at %s \n" %(start_time_hr))
+trackfile.write("Starting process at %s \n" %(get_formatted_time()))
 trackfile.close()
 
 # Structure holding the final cubes for every pair [obsid,camera]
@@ -92,25 +103,20 @@ finalCubeList = []
 
 # Run pipeline over obs
 for i in range(len(obs_list)):
-    camera='red'
+    camera = 'red'
     # Next, get the data
-    observations_dictionary["obs_{0}".format(observations_dict.keys()[i])] = getObservation(observations_dict.keys()[i],
-                                                                                            useHsa = 1)
+    obs_key = "obs_{0}".format(observations_dict.keys()[i])
+    observations_dictionary[obs_key] = getObservation(observations_dict.keys()[i], useHsa = 1)
 
-    # print outs to keep you up to date with progress
-    actual_time = time.time()
-    actual_time_hr = datetime.datetime.fromtimestamp(actual_time)
-    actual_time_hr = str(actual_time_hr)
-    
     trackfile = open(trackfilename, 'a')
-    trackfile.write("Processing observation " + str(observations_dict.keys()[i]) +
-                    " with camera " + camera + " at " + str(actual_time_hr) +
-                    "\n")
+    trackfile.write("Processing observation " +
+                    str(observations_dict.keys()[i]) + " with camera " + camera +
+                    " at " + str(get_formatted_time()) + "\n")
     trackfile.close()
 
     runPacsSpg(cameraList=[camera],
                obsIn=observations_dictionary["obs_{0}".format(observations_dict.keys()[i])])
-    
+
     name = 'obs_' + str(observations_dict.keys()[i])
     saveObservation(observations_dictionary["obs_{0}".format(observations_dict.keys()[i])],
                     poolLocation=pool_dir, poolName=name)
@@ -126,7 +132,7 @@ for i in range(len(obs_list)):
     duration_m = int(duration/60)
     duration_s = duration - duration_m*60
 
-    camera='red'
+    camera = 'red'
     trackfile = open(trackfilename, 'a')
     trackfile.write('End ' + str(observations_dict.keys()[i]) + " " + camera +
                     ' Duration: ' + str(duration_m) + ' m ' +

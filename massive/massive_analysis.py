f2b847dbf407e4a2aaa4f828edc4cac025a8ef7e
Samuel Góngora García <s.gongoragarcia@gmail.com>
2016-10-06 13:11:26 +0200

    Update massive_analysis.py

--- massive/massive_analysis.py
+++ massive/massive_analysis.py
@@ -1,8 +1,10 @@
 from os import getenv, path, mkdir
 from time import time
-import datetime
+from datetime import datetime
 from csv import reader
 from string import uppercase
+from massive_aux import get_formatted_time, save_exception
+from massive_aux import create_dictionary, populate_obs
 
 #  coding = utf-8
 #
@@ -24,54 +26,8 @@ from string import uppercase
 #  If not, see <http://www.gnu.org/licenses/>.
 #
 
-
-def get_formatted_time():
-    """ Return formatted time function
-
-    """
-    time_hr = datetime.datetime.fromtimestamp(time())
-    time_hr = str(time_hr)
-
-    return time_hr
-
-
-def save_exception(exception):
-    """ Save expection to file
-
-    """
-    print exception
-
-    
-def create_dictionary(obs_list):
-    """ Create dictionary from observations list
-    
-    """
-    observations_dict = {}
-    i = 0
-    j = 0
-    k = 0
-    w = 0
-    for i in range(len(obs_list)):
-        if j == int(len(list(uppercase))):
-            j = 0
-            k = k + 1
-
-        if k == int(len(list(uppercase))):
-            k = 0
-            w = w + 1
-
-        obs_dict[obs_list[i]] = 'SED' +
-                                list(uppercase)[j] +
-                                list(uppercase)[k] +
-                                list(uppercase)[w]
-        j = j + 1
-        
-    return obs_dict
-
-
 # Load directories
 # In our case we assumed that ...
-
 home_dir = getenv("HOME")
 working_dir = str(home_dir) + '/hcss/workspace/Red-leak/'
 pool_dir = str(working_dir) + 'pools/'
@@ -80,97 +36,96 @@ csv_obs = str(working_dir) + 'obs_ids.csv'
 
 save_obs = False
 
-start_time = time.time()
-start_time_hr = datetime.datetime.fromtimestamp(start_time)
-start_time_hr = str(start_time_hr)
+run_massive():
+    start_time = time.time()
+    start_time_hr = datetime.datetime.fromtimestamp(start_time)
+    start_time_hr = str(start_time_hr)
 
-if (not path.exists(working_dir)):
-    mkdir(working_dir)
-if (not path.exists(pool_dir)):
-    mkdir(pool_dir)
-if (not path.exists(zips_dir)):
-    mkdir(zips_dir)
+    if (not path.exists(working_dir)):
+        mkdir(working_dir)
+    if (not path.exists(pool_dir)):
+        mkdir(pool_dir)
+    if (not path.exists(zips_dir)):
+        mkdir(zips_dir)
 
-# Populate list from csv file
-obs_list = []
-with open(str(csv_obs), 'rb') as f:
-    row_reader = reader(f, delimiter=',')
-    for row in row_reader:
-        obs_list.append(row[1])
+    obs_list = populate_obs(csv_obs)
 
-
-# Create file for tracking the progress
-trackfilename = working_dir + "RedLeakMultiObs.txt"
-trackfile = open(trackfilename, 'w')
-trackfile.write("Starting process at %s \n" %(get_formatted_time()))
-trackfile.close()
-
-# Structure holding the final cubes for every pair [obsid,camera]
-observations_dictionary = create_dictionary(obs_list)
-finalCubeList = []
-
-# TODO try-except raising all the problems to external file
-# Run pipeline over obs
-for i in range(len(obs_list)):
-    camera = 'red'
-
-    obs_number = observations_dict.keys()[i]
-    observations_dictionary[obs_number] = getObservation(obs_number, useHsa=1)
-
-    trackfile = open(trackfilename, 'a')
-    trackfile.write("Processing observation " +
-                    str(obs_number) + " with camera " + camera +
-                    " at " + str(get_formatted_time()) + "\n")
+    # Create file for tracking the progress
+    trackfilename = working_dir + "RedLeakMultiObs.txt"
+    trackfile = open(trackfilename, 'w')
+    trackfile.write("Starting process at %s \n" %(get_formatted_time()))
     trackfile.close()
 
-    runPacsSpg(cameraList=[camera],
-               obsIn=observations_dictionary[obs_number])
+    # Structure holding the final cubes for every pair [obsid,camera]
+    observations_dictionary = create_dictionary(obs_list)
+    finalCubeList = []
 
-    name = 'obs_' + str(observations_dict.keys()[i])
+    # TODO try-except raising all the problems to external file
+    # Run pipeline over obs
+    for i in range(len(obs_list)):
+        camera = 'red'
 
-    try:
-        print "Saving observation: ", obs_number
-        saveObservation(observations_dictionary[obs_number],
-                        poolLocation=pool_dir, poolName=name)
-        print "Observation saved"
-    except Exception as e:
-        save_exception(e)
-        print "An exception was raised during saving process ", e
+        obs_number = observations_dict.keys()[i]
+        observations_dictionary[obs_number] = getObservation(obs_number,
+                                                             useHsa=1)
+
+        trackfile = open(trackfilename, 'a')
+        trackfile.write("Processing observation " +
+                        str(obs_number) + " with camera " + camera +
+                        " at " + str(get_formatted_time()) + "\n")
+        trackfile.close()
+
+        runPacsSpg(cameraList=[camera],
+                   obsIn=observations_dictionary[obs_number])
+
+        name = 'obs_' + str(observations_dict.keys()[i])
+
+        try:
+            print "Saving observation: ", obs_number
+            saveObservation(observations_dictionary[obs_number],
+                            poolLocation=pool_dir, poolName=name)
+            print "Observation saved"
+        except Exception as e:
+            save_exception(e)
+            print "An exception was raised during saving process ", e
     
-    try:
-        print "Exporting observation: ", obs_number
-        exportObservation(pool=PoolManager.getPool(obs_number), 
-                          urn=obs_number + ":herschel.ia.obs.ObservationContext:0",
-                          dirout=pool_dir + "Export/" + obs_number)
-    except Exception as e:
-        save_exception(e)
-        print "An exception was raised during exporting process ", e
+        try:
+            print "Exporting observation: ", obs_number
+            exportObservation(pool=PoolManager.getPool(obs_number), 
+                              urn=obs_number + ":herschel.ia.obs.ObservationContext:0",
+                              dirout=pool_dir + "Export/" + obs_number)
+        except Exception as e:
+            save_exception(e)
+            print "An exception was raised during exporting process ", e
 
-    try:
-        print "Compressing file for observation: ", obs_number
-        compress(inputpath=pool_dir + "Export/" + obs_number,
-                 archive=pool_dir + obs_number + ".tgz", compression="TGZ")
+        try:
+            print "Compressing file for observation: ", obs_number
+            compress(inputpath=pool_dir + "Export/" + obs_number,
+                     archive=pool_dir + obs_number + ".tgz", compression="TGZ")
         
-    except Exception as e:
-        save_exception(e)
-        print "An exception was raised during compression process", e
+        except Exception as e:
+            save_exception(e)
+            print "An exception was raised during compression process", e
 
-    duration = time.time() - actual_time
+        duration = time.time() - actual_time
+        duration_m = int(duration/60)
+        duration_s = duration - duration_m*60
+
+        camera = 'red'
+        trackfile = open(trackfilename, 'a')
+        trackfile.write('End ' + obs_number + " " + camera +
+                        ' Duration: ' + str(duration_m) + ' m ' +
+                    str(duration_s) + ' s ' + '\n')
+        trackfile.close()
+
+    duration = time.time() - start_time
     duration_m = int(duration/60)
     duration_s = duration - duration_m*60
 
-    camera = 'red'
     trackfile = open(trackfilename, 'a')
-    trackfile.write('End ' + obs_number + " " + camera +
-                    ' Duration: ' + str(duration_m) + ' m ' +
-                    str(duration_s) + ' s ' + '\n')
+    trackfile.write("END Total Duration: " +
+                str(duration_m) + ' m ' + str(duration_s) + ' s ' + "\n")
     trackfile.close()
 
-duration = time.time() - start_time
-duration_m = int(duration/60)
-duration_s = duration - duration_m*60
-
-trackfile = open(trackfilename, 'a')
-trackfile.write("END Total Duration: " +
-                str(duration_m) + ' m ' + str(duration_s) + ' s ' + "\n")
-trackfile.close()
+if __name__ == "__main__":
+    run_massive()
